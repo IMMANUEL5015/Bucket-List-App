bb4c18f3aaec5873c7b0ac8890a4a84c0f19e172
IMMANUEL5015 <immanueldiai@gmail.com>
2019-11-21 23:36:03 +0100

    Attempt to fix testing issues with database connection

--- server/mongodb/mongodb.connect.js
+++ server/mongodb/mongodb.connect.js
@@ -1,15 +1,10 @@
 const mongoose = require('mongoose');
 
 async function connect(){
-    try{
-        await mongoose.connect("mongodb+srv://Immanuel50:chukaglorY55@cluster0-ggn2t.mongodb.net/bucketlist_App?retryWrites=true&w=majority", {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-    } catch(err){
-        console.log(err);
-        console.log('error in connecting to mongodb');
-    }
+    await mongoose.connect("mongodb+srv://Immanuel50:chukaglorY55@cluster0-ggn2t.mongodb.net/bucketlist_App?retryWrites=true&w=majority", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
 }
 
 module.exports = { connect };
